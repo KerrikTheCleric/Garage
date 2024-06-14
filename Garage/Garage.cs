7044eeee1f9f1de405338ac53abe1f46fa52b8fc
Victor Gustafsson <kerrik.vg@gmail.com>
2024-06-14 12:52:54 +0200

    Rough first step

--- Garage/Garage.cs
+++ Garage/Garage.cs
@@ -1,10 +1,51 @@
-﻿using System;
+﻿using GarageTask.Vehicles;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Garage {
-    internal class Garage {
+namespace GarageTask {
+    internal class Garage<T> : IEnumerable<T> where T : IVehicle{
+        private T[] _vehicles;
+
+
+        public Garage (int size) {
+            _vehicles = new T[size];
+            Vehicles = _vehicles;
+        }
+
+        public void AddVehicle(IVehicle v) {
+            /*for (int i = 0; i < _vehicles.Length; ++i) {
+                if (Vehicles[i] = null) {
+
+                }
+            }*/
+
+            Vehicles[0] = (T)v;
+
+            for (int i = 0; i < Vehicles.Length; ++i) {
+                Console.WriteLine(Vehicles[i]);
+            }
+
+        }
+
+        private T[] Vehicles { 
+            get => _vehicles; 
+            set => _vehicles = value; 
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+
+            for(int i = 0; i < _vehicles.Length; ++i)
+            {
+                yield return _vehicles[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
     }
 }
--- Garage/Program.cs
+++ Garage/Program.cs
@@ -1,7 +1,15 @@
-﻿namespace Garage {
+﻿using GarageTask.Vehicles;
+
+namespace GarageTask {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            Boat b1 = new Boat();
+            Boat b2 = new Boat();
+            Boat b3 = new Boat();
+
+            Garage<IVehicle> garage = new Garage<IVehicle>(1);
+
+            garage.AddVehicle(b1);
         }
     }
 }
