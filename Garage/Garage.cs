379e56d2333101f591389b7c44329229833cf11d
Victor Gustafsson <kerrik.vg@gmail.com>
2024-06-18 12:20:00 +0200

    Registration check for AddVehicle, rewrite of PrintGarage

--- Garage/Garage.cs
+++ Garage/Garage.cs
@@ -18,32 +18,77 @@ namespace GarageTask {
             RemainingGarageSpaces = size;
         }
 
-        public bool AddVehicle(IVehicle newVehicle) {
+        private T[] Vehicles {
+            get => _vehicles;
+            set => _vehicles = value;
+        }
+
+        private int TotalGarageSpaces { get; set; }
+
+        private int RemainingGarageSpaces { get; set; }
+
+        public int AddVehicle(IVehicle newVehicle) {
 
             if (RemainingGarageSpaces == 0) {
-                return false;
+                return 1;
             }
 
-            // TODO: Change loop to traverse entire array looking for registration duplicates and save last free spot to use
+            int freeSpace = -1;
 
             for (int i = 0; i < Vehicles.Length; ++i) {
-                if (Vehicles[i] == null) {
-                    Vehicles[i] = (T)newVehicle;
 
-                    RemainingGarageSpaces--;
-
-                    return true;
+                if (Vehicles[i] != null) {
+                    if (newVehicle.GetRegistrationNumber() == Vehicles[i].GetRegistrationNumber()) {
+                        return 2;
+                    }
+                } else if (freeSpace == -1) {
+                    freeSpace = i;
                 }
             }
-            return false;
+
+            Vehicles[freeSpace] = (T)newVehicle;
+            RemainingGarageSpaces--;
+            return 0;
         }
+
+        // RemoveVehicleAtSpotNumber
+
+        // RemoveVehicleWithRegistrationNumber
+
+        public IVehicle FindVehicleAtSpotNumber(int index) {
+
+            if (TotalGarageSpaces > index && index >= 0) {
+                return Vehicles[index];
+            }
+            return null;
+        }
+
+        // FindVehicleWithRegistrationNumber
+
+        // Move to UI
 
         public void PrintGarage(int columns) {
 
             int columnsPrinted = 0;
             int spacesPrinted = 0;
 
-            foreach (IVehicle v in this) {
+            for (int i = 0; i < TotalGarageSpaces; i++) {
+                if (Vehicles[i] == null) {
+                    Console.Write($"{spacesPrinted + 1}. [ ] ");
+                } else {
+                    Console.Write($"{spacesPrinted + 1}. [V] ");
+                }
+                columnsPrinted++;
+                spacesPrinted++;
+
+                if (columnsPrinted == columns) {
+                    Console.Write("\n");
+                    columnsPrinted = 0;
+                }
+            }
+
+
+            /*foreach (IVehicle v in this) {
                 Console.Write($"{spacesPrinted + 1}. [V] ");
                 columnsPrinted++;
                 spacesPrinted++;
@@ -64,19 +109,9 @@ namespace GarageTask {
                     Console.Write("\n");
                     columnsPrinted = 0;
                 }
-            }
+            }*/
             Console.WriteLine("");
         }
-
-        private T[] Vehicles {
-            get => _vehicles;
-            set => _vehicles = value;
-        }
-
-        private int TotalGarageSpaces { get; set; }
-
-        private int RemainingGarageSpaces { get; set; }
-
 
         public IEnumerator<T> GetEnumerator() {
             for (int i = 0; i < Vehicles.Length; ++i) {
--- Garage/Program.cs
+++ Garage/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Garage;
 using GarageTask.Vehicles;
+using System.Resources;
+using System.Text.Json;
 
 namespace GarageTask {
     internal class Program {
@@ -14,11 +16,14 @@ namespace GarageTask {
                 Console.WriteLine("Error: " + e.Message);
             }*/
 
+            //string jsonString = File.ReadAllText("Resources\\starter_vehicles.json");
+            //Airplane a = JsonSerializer.Deserialize<Airplane>(jsonString);
+
 
             try {
-                Boat b1 = new Boat("aBa123", Colour.Orange, 400.6, 2320.8, 60);
-                Airplane a1 = new Airplane("TBA555", Colour.Yellow, 0.0, 0.0, 0, 8);
-                Motorcycle m1 = new Motorcycle("KLG899", Colour.Purple, 0.0, 0.0, 0);
+                Boat b1 = new Boat("aBa123", Colour.Orange, 400.6, 2320.8, 60, true);
+                Airplane a1 = new Airplane("TBA555", Colour.Yellow, 0.0, 0.0, 0, 'B',8);
+                Motorcycle m1 = new Motorcycle("KLG899", Colour.Purple, 0.0, 0.0, 0, false);
 
                 Garage<IVehicle> garage = new Garage<IVehicle>(45);
 
@@ -33,6 +38,22 @@ namespace GarageTask {
                 }
 
                 garage.PrintGarage(10);
+
+                IVehicle result = garage.FindVehicleAtSpotNumber(44);
+
+                Console.WriteLine(result.GetRegistrationNumber());
+                Console.WriteLine(result.GetType());
+                Console.WriteLine(result is Boat);
+
+                switch (result.GetType().Name) {
+
+                    case "Motorcycle":
+                        Console.WriteLine("Is a motorcycle!");
+                        break;
+                    default:
+                        break;
+                }
+
 
             } catch (Exception e) {
                 Console.WriteLine("Error: " + e.Message);
